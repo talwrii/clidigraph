cbd27ff18c1fca0566ab8b4fe158980dd6921a73
Tal Wrii <talwrii@gmail.com>
2017-11-16 06:01:23 +0100

    fastentrypoints and alternative shorter name

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
+
+import sys
+try:
+    # setuptools entry point is slow
+    #  if we have festentrypoint use
+    #  a fast entry point
+    import fastentrypoints
+except ImportError:
+    sys.stdout.write('Not using fastentrypoints\n')
+    pass
+
 import setuptools
 import os
+
 
 HERE = os.path.dirname(__file__)
 
@@ -15,7 +27,7 @@ setuptools.setup(
     packages=['clidigraph'],
     long_description='See https://github.com/talwrii/clidigraph',
     entry_points={
-        'console_scripts': ['clidigraph=clidigraph.clidigraph:main']
+        'console_scripts': ['clidigraph=clidigraph.clidigraph:main', 'clidi=clidigraph.clidigraph:main']
     },
     classifiers=[
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)"
